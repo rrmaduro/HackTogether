619539907a5eeaf5d8cfaccd3b4b96cb2dc2d90f
afonm <127105226+afonm@users.noreply.github.com>
2024-11-09 18:01:03 +0000

    correcting an error

--- hacktogether-template/src/App.tsx
+++ hacktogether-template/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/mainNavbar';
-import Locations from './components/Locations/locations'
 
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Locations/>
+      <Navbar/>
     </div>
   );
 }
