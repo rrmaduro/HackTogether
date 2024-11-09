b3437dd59020426676179a81c391026eddcb2326
afonm <127105226+afonm@users.noreply.github.com>
2024-11-09 16:21:34 +0000

    signup page created
    signup component created
    signup page created

--- hacktogether-template/src/App.tsx
+++ hacktogether-template/src/App.tsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar/mainNavbar';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+      <Navbar/>
     </div>
   );
 }
--- hacktogether-template/src/components/Signup/signup.tsx
+++ hacktogether-template/src/components/Signup/signup.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CircleUserRound } from 'lucide-react';
 
-export default function Login() {
+export default function Signup() {
     return (
     <nav className="container-fluid min-vh-100 d-flex align-items-center justify-content-center ">
         <div className="bg-light p-5 rounded shadow text-center">
@@ -10,6 +10,14 @@ export default function Login() {
             </div>
             <div className='login-form'>
                 <form>
+                    <div className='mb-3'>
+                        <label htmlFor="name" className='form-label'>Name</label>
+                        <input type="text" className='form-control' name="name" id="person-name" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="name" className='form-label'>Birthday</label>
+                        <input type="date" className='form-control' name="name" id="person-name" aria-describedby="emailHelp"/>
+                    </div>
                     <div className="mb-3">
                       <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
@@ -17,6 +25,10 @@ export default function Login() {
                     <div className="mb-3">
                       <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                       <input type="password" className='form-control' id='exampleInputPassword1' />
+                    </div>
+                    <div className="mb-3 form-check">
+                      <input type="checkbox" name="form-check-input" id="exampleCheck1" />
+                      <label className="form-check-label" htmlFor="exampleCheck1">Agree with terms</label>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
