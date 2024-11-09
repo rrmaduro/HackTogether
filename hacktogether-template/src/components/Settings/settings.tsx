c3ac45d6e6ce4ca9656e69c23ab909adf011771b
afonm <127105226+afonm@users.noreply.github.com>
2024-11-09 23:12:17 +0000

    settings page programmed, not design

---
+++ hacktogether-template/src/components/Settings/settings.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CircleUserRound } from 'lucide-react';
+
+export default function Settings() {
+    return (
+    <nav className="container-fluid min-vh-100 d-flex align-items-center justify-content-center ">
+        <div className="bg-light p-5 rounded shadow text-center">
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                <input type="email" name="form-control" id="exampleFormControlInput1" />
+            </div>
+            <div className='mb-3'>
+                <label htmlFor="usernameSetting" className='form.label'>username</label>
+                <input type="text" name="usernameSettingsInput" id="usernameSettings" />
+            </div>
+            <div className='mb-3'>
+                <label htmlFor="locationSetting" className='form.label'>username</label>
+                <input type="text" name="locationSettingsInput" id="locationSettings" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
+                <textarea name="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+        
+      </nav>
+    );
+}
