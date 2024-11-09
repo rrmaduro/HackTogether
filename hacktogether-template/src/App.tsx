b0eaf4348f865eed1c3b7c8bec305f726e50690d
afonm <127105226+afonm@users.noreply.github.com>
2024-11-09 22:21:36 +0000

    correct errors

--- hacktogether-template/src/App.tsx
+++ hacktogether-template/src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/mainNavbar';
 import MainPage from '@components/MainPage/MainPage';
-import About from '@components/About/about'
 
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar/>
-      <About/>
+      <MainPage/>
     </div>
   );
 }
