d25754bfa5cadab288f680890c8f855747cd57f6
afonm <127105226+afonm@users.noreply.github.com>
2024-11-09 23:49:42 +0000

    change app.tsx

--- hacktogether-template/src/App.tsx
+++ hacktogether-template/src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/mainNavbar';
 import MainPage from '@components/MainPage/MainPage';
-import Settings from '@components/Settings/settings';
 
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar/>
-      <Settings/>
+      <MainPage/>
     </div>
   );
 }
